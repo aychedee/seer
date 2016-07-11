2aa69a76f0824363879379b55580a92c7f21f753
Hansel Dunlop <hansel@interpretthis.org>
2016-07-12 00:32:58 +0100

    Connected neurons can be activated

--- seer.go
+++ seer.go
@@ -1,12 +1,23 @@
 package main
 
+type ActivationFunction func(n float64) float64
+
 type Synapse struct {
 	Weight float64
+	In     float64
+	Out    float64
+}
+
+func (s *Synapse) Activate(value float64) {
+	s.In = value
+	s.Out = value * s.Weight
 }
 
 type Neuron struct {
-	Outgoing []*Synapse
-	Incoming []*Synapse
+	Out                float64
+	Outgoing           []*Synapse
+	Incoming           []*Synapse
+	ActivationFunction ActivationFunction
 }
 
 func (n *Neuron) Connect(neuron *Neuron, weight float64) {
@@ -15,8 +26,23 @@ func (n *Neuron) Connect(neuron *Neuron, weight float64) {
 	neuron.Incoming = append(neuron.Incoming, synapse)
 }
 
-func NewNeuron() *Neuron {
-	return &Neuron{}
+func (n *Neuron) Calculate() {
+
+	var sum float64
+	for _, s := range n.Incoming {
+		sum += s.Out
+	}
+
+	n.Out = n.ActivationFunction(sum)
+
+	for _, s := range n.Outgoing {
+		s.Activate(n.Out)
+	}
+
+}
+
+func NewNeuron(af ActivationFunction) *Neuron {
+	return &Neuron{ActivationFunction: af}
 }
 
 func main() {
--- seer_test.go
+++ seer_test.go
@@ -1,10 +1,13 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestConnectTwoNeurons(t *testing.T) {
-	neuron1 := NewNeuron()
-	neuron2 := NewNeuron()
+	neuron1 := NewNeuron(math.Tanh)
+	neuron2 := NewNeuron(math.Tanh)
 
 	neuron1.Connect(neuron2, 1.0)
 
@@ -19,4 +22,26 @@ func TestConnectTwoNeurons(t *testing.T) {
 	if neuron1.Outgoing[0].Weight != 1.0 {
 		t.Error("Connection not assigned correct weight")
 	}
+}
+
+func TestFireConnectedNeurons(t *testing.T) {
+	neuron1 := NewNeuron(math.Tanh)
+	neuron2 := NewNeuron(math.Tanh)
+	neuron3 := NewNeuron(math.Tanh)
+	neuron4 := NewNeuron(math.Tanh)
+
+	neuron1.Connect(neuron2, 10.0)
+	neuron2.Connect(neuron3, 5.0)
+	neuron3.Connect(neuron4, 5.0)
+
+	neuron1.Incoming = []*Synapse{&Synapse{Out: 2}}
+	neuron1.Calculate()
+	neuron2.Calculate()
+	neuron3.Calculate()
+	neuron4.Calculate()
+
+	if neuron4.Out < 0.9 {
+		t.Errorf("Out not correct, was %f", neuron4.Out)
+	}
+
 }
